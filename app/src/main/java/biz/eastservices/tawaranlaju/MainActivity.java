867c17e624e5c5c4f5f108654ee7fef412e5f54c
Sabeeh Ul Hussnain <mr.hussnain.it@gmail.com>
2018-01-26 18:34:49 +0500

    mobile authentication issue fixed.

--- app/src/main/java/biz/eastservices/tawaranlaju/MainActivity.java
+++ app/src/main/java/biz/eastservices/tawaranlaju/MainActivity.java
@@ -1,13 +1,17 @@
 package biz.eastservices.tawaranlaju;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.ErrorCodes;
 import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -17,14 +21,13 @@ import java.util.Collections;
 public class MainActivity extends AppCompatActivity {
     private static final int RC_SIGN_IN = 123;
     FirebaseAuth mAuth;
+    private String TAG = MainActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mAuth = FirebaseAuth.getInstance();
-    }
-    @Override
-    protected void onStart() {
-        super.onStart();
+
         FirebaseAuth mAuth = FirebaseAuth.getInstance();
         if (mAuth.getCurrentUser() != null) {
             // already signed in
@@ -33,20 +36,30 @@ public class MainActivity extends AppCompatActivity {
             finish();
         } else {
             // not signed in
-            startActivityForResult(
-
-                    AuthUI.getInstance()
-                            .createSignInIntentBuilder()
-                            .setIsSmartLockEnabled(!BuildConfig.DEBUG)
-                            .setAvailableProviders(
-                                    Collections.singletonList(
-                                            new AuthUI.IdpConfig.PhoneBuilder().setDefaultCountryIso("my").build()
-                                    )
-                            )
-                            .build(),
-                    RC_SIGN_IN);
+            startMobileAuthentication();
         }
     }
+
+    private void startMobileAuthentication() {
+        startActivityForResult(
+
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setIsSmartLockEnabled(!BuildConfig.DEBUG)
+                        .setAvailableProviders(
+                                Collections.singletonList(
+                                        new AuthUI.IdpConfig.PhoneBuilder().setDefaultCountryIso("pk").build()
+                                )
+                        )
+                        .build(),
+                RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -57,11 +70,16 @@ public class MainActivity extends AppCompatActivity {
                 String user_id = mAuth.getCurrentUser().getPhoneNumber();
                 //String user_phone = mAuth.getCurrentUser().getPhoneNumber();
                 DatabaseReference current_user_db = FirebaseDatabase.getInstance().getReference().child("Users").child("Customers").child(user_id);
-                current_user_db.setValue(true);
+                current_user_db.setValue(FirebaseAuth.getInstance().getCurrentUser().getPhoneNumber()).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Log.d(TAG, "onComplete: " + task.isSuccessful());
+                    }
+                });
 
                 Intent intent = new Intent(MainActivity.this, CustomerActivity.class);
                 startActivity(intent);
-
+                finish();
                 return;
             }
         } else {
@@ -84,6 +102,8 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "UNKNOWN ERROR", Toast.LENGTH_SHORT).show();
                 return;
             }
+
+            startMobileAuthentication();
         }
 
         Toast.makeText(MainActivity.this, "NO INTERNET CONNECTION", Toast.LENGTH_SHORT).show();
